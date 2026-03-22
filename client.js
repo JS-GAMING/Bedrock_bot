71b29bfdadd7d872f1e62dcbdad32ad310c87c6a
JS-GAMING <84761559+JS-GAMING@users.noreply.github.com>
2026-03-22 11:39:57 +0530

    Create client.js

---
+++ client.js
@@ -0,0 +1,30 @@
+const bedrock = require('bedrock-protocol')
+const config = require('../config')
+const { setStatus, startSession, endSession } = require('./state')
+const { startActions } = require('./actions')
+
+function createBot() {
+  setStatus("Connecting")
+
+  const client = bedrock.createClient(config.server)
+
+  client.on('join', () => {
+    console.log('✅ Joined')
+    setStatus("Online")
+    startSession()
+
+    startActions(client, config.server.username)
+  })
+
+  client.on('disconnect', () => {
+    console.log('❌ Disconnected')
+    setStatus("Reconnecting")
+    endSession()
+
+    setTimeout(createBot, 5000)
+  })
+
+  client.on('error', err => console.log(err))
+}
+
+module.exports = { createBot }
